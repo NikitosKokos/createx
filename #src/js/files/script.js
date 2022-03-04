d1e7ea0b8aa1c95d4562fe31e724920b8fd13173
Nikita Tsykunov <nikitaodessafreelance@gmail.com>
2022-03-04 23:28:28 +0200

    added section partners and testimonials

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+   // sliders
     const sliderPortfolio = new Swiper('.portfolio-section__items', {
        slidesPerView: 3,
        spaceBetween: 30,
@@ -7,6 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
        navigation: {
           nextEl: '.portfolio-section__next',
           prevEl: '.portfolio-section__prev',
+       },
+    });
+    const sliderTestimonials = new Swiper('.testimonials__items', {
+       slidesPerView: 1,
+       spaceBetween: 30,
+       loop: true,
+       speed: 800,
+       // Arrows
+       navigation: {
+          nextEl: '.testimonials__next',
+          prevEl: '.testimonials__prev',
        },
     });
 
--- createx/js/app.js
+++ createx/js/app.js
@@ -2,6 +2,7 @@ function email_test(input) {
 	return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
 }
 document.addEventListener('DOMContentLoaded', () => {
+   // sliders
     const sliderPortfolio = new Swiper('.portfolio-section__items', {
        slidesPerView: 3,
        spaceBetween: 30,
@@ -10,6 +11,17 @@ document.addEventListener('DOMContentLoaded', () => {
        navigation: {
           nextEl: '.portfolio-section__next',
           prevEl: '.portfolio-section__prev',
+       },
+    });
+    const sliderTestimonials = new Swiper('.testimonials__items', {
+       slidesPerView: 1,
+       spaceBetween: 30,
+       loop: true,
+       speed: 800,
+       // Arrows
+       navigation: {
+          nextEl: '.testimonials__next',
+          prevEl: '.testimonials__prev',
        },
     });
 
